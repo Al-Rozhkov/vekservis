364789cd87c2be1d2895b51e17bfed4485ab499e
Al-Rozhkov <rozhkov-aa@yandex.ru>
2018-02-08 13:43:45 +0400

    Improve favicon with transparent png

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'ВЭК СЕРВИС' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:400,600&amp;subset=cyrillic' }
     ]
   },

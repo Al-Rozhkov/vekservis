a6ed0a8b9f6df43ec7a079bb0d444445ddb08d36
Alexander Rozhkov <rozhkov-aa@yandex.ru>
2019-07-23 02:52:00 +0300

    Dirty experiment

--- api/order/index.js
+++ api/order/index.js
@@ -2,12 +2,25 @@ module.exports = async (req, res) => {
   // CORS preflight approve
   if (req.method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Methods', 'POST')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
     res.setHeader('Access-Control-Max-Age', '86400')
     res.statusCode = 200
     res.end()
+    
+    const body = ''
+    req.on('data', chunk => {
+      body += chunk.toString() // convert Buffer to string
+    })
+    req.on('end', () => {
+      console.log(body)
+      res.end('ok')
+    })
+    return
   }
-  
+
   const { body } = req
   if (body && body.name) {
     return res.end(`Hello ${body.name}, you just parsed the request body!`)

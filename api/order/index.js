3f5c5cbe21646ccec692449af1bb080d140fb67d
Alexander Rozhkov <rozhkov-aa@yandex.ru>
2019-07-23 02:15:19 +0300

    Try to not return function

--- api/order/index.js
+++ api/order/index.js
@@ -1,12 +1,11 @@
 module.exports = async (req, res) => {
   // CORS preflight approve
   if (req.method === 'OPTIONS') {
-    res.writeHead(200, {
-      'Access-Control-Allow-Methods': 'POST',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-      'Access-Control-Max-Age': '86400'
-    })
-    return res.end()
+    res.setHeader('Access-Control-Allow-Methods', 'POST')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.setHeader('Access-Control-Max-Age', '86400')
+    res.statusCode = 200
+    res.end()
   }
   
   const { body } = req
